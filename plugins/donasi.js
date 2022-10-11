963b719a210c29564df359f6b3758c351f52e0b3
LEVI-GANZ-BOTZ <111663350+mytobot@users.noreply.github.com>
2022-10-11 16:44:49 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+│ •  [085172446692]
+│ •  [082131369002]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
