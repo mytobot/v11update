973adaf4fd9715d9fd31ddb3cbdb6245020563fc
LEVI-GANZ-BOTZ <111663350+mytobot@users.noreply.github.com>
2022-10-11 16:43:30 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['6287898307350'],
-  ['6287898307350'],
-  ['6287898307350', 'Owner', true]
+  ['6285172446692'],
+  ['6285172446692'],
+  ['6285172446692', '𝙻𝙴𝚅𝙸-𝙶𝙰𝙽𝚉', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@penta_bot'
-  var sticker_author = 'penta'
+  var sticker_name = '@𝙻𝙴𝚅𝙸-𝙼𝙾𝙳𝚂-𝙶𝙰𝙽𝚉'
+  var sticker_author = '𝙻𝙴𝚅𝙸-𝙶𝙰𝙽𝚉'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© Gempy'
+global.wm = '© 𝙻𝙴𝚅𝙸-𝙶𝙰𝙽𝚉'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
